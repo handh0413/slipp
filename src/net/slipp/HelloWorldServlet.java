eb873d4f7a0ece1765cc8d330bc4dde54ffa752d
Donghee Han <handh0413@naver.com>
2021-07-29 20:30:21 +0900

    Servlet에서 JSP로의 변천사

--- src/net/slipp/HelloWorldServlet.java
+++ src/net/slipp/HelloWorldServlet.java
@@ -15,7 +15,17 @@ public class HelloWorldServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = resp.getWriter();
-		out.println("Hello World!");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">");
+		out.println("<title>SLiPP</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hello World</h1>");
+		out.println("여기는 Servlet");
+		out.println("</body>");
+		out.println("</html>");
 	}
 }
