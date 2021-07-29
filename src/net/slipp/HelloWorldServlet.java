cb8c5a9c234bf8ae1c4450036a3231d851a465b6
Donghee Han <handh0413@naver.com>
2021-07-29 20:12:02 +0900

    Servlet 3.0에서 Annotation 활용한 매핑

--- src/net/slipp/HelloWorldServlet.java
+++ src/net/slipp/HelloWorldServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet(name = "HelloWorld", urlPatterns = { "/helloworld", "/hello", "/hello/world" })
 public class HelloWorldServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
