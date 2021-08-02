01a2f975087ecc93943366652d1c02d4f2be4436
Donghee Han <handh0413@naver.com>
2021-08-02 20:03:08 +0900

    사용자 데이터 저장 및 sendRedirect 사용

---
+++ src/net/slipp/db/DataBase.java
@@ -0,0 +1,18 @@
+package net.slipp.db;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.slipp.user.User;
+
+public class DataBase {
+	private static Map<String, User> users = new HashMap<>();
+	
+	public static void addUser(User user) {
+		users.put(user.getUserId(), user);
+	}
+	
+	public static User findById(String userId) {
+		return users.get(userId);
+	}
+}
