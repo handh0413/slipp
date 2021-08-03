ece2a83a27239679904ad9dee780d19412ef0270
Donghee Han <handh0413@naver.com>
2021-08-03 23:41:19 +0900

    회원가입 화면과 개인정보 수정 화면의 중복 제거

---
+++ src/net/slipp/user/CreateFormUserServlet.java
@@ -0,0 +1,23 @@
+package net.slipp.user;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/users/create")
+public class CreateFormUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setAttribute("user", new User());
+		RequestDispatcher rd = req.getRequestDispatcher("/form.jsp");
+		rd.forward(req, resp);
+	}
+
+}
--- src/net/slipp/user/User.java
+++ src/net/slipp/user/User.java
@@ -8,6 +8,10 @@ public class User {
 	private String name;
 	private String email;
 	
+	public User() {
+		super();
+	}
+
 	public User(String userId, String password, String name, String email) {
 		super();
 		this.userId = userId;
